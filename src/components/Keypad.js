329ebb5f0a8b75be7ada77bcf8d5545b0f8a76ec
Farida NDiaye <faridandiaye60@gmail.com>
2023-08-04 09:50:14 -0400

    EVENT HANDLING LAB

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    const handleInputChange = () => {
+        console.log('Entering password...');
+      };
     return (
-        <div></div>
-    )
+        <div>
+      <input type="password" onChange={handleInputChange} />
+      </div>
+    );
 }
 
 export default Keypad;
+
